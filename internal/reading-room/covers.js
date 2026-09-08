636a726d463a90e7efb46036aad867648bba1376
Ballzatram <devinmgallemore@gmail.com>
2026-09-08 13:27:24 -0400

    Add pinned cover manifest for Reading Room

---
+++ internal/reading-room/covers.js
@@ -0,0 +1,18 @@
+const COVER_OVERRIDES={
+  "The Gunslinger":"https://books.google.com/books/content?id=X7ZJaea0P7cC&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "Fingerprints of the Gods":"https://books.google.com/books/content?id=bABZAAAACAAJ&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "An Unquiet Mind":"https://books.google.com/books/content?id=1TlF6SaJz8UC&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "The Art of Peace":"https://books.google.com/books/content?id=2jKQEAAAQBAJ&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "Never Split the Difference":"https://books.google.com/books/content?id=S6r5wAEACAAJ&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "It":"https://books.google.com/books/content?id=4e5BYgEACAAJ&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "Animal Farm":"https://books.google.com/books/content?id=JyJbAAAAMAAJ&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "Doctor Zhivago":"https://books.google.com/books/content?id=HwW3vwEACAAJ&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "Of Mice and Men":"https://books.google.com/books/content?id=nE_Si_bv-W4C&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "The Godfather":"https://books.google.com/books/content?id=x8fdmWiV97YC&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "1984":"https://books.google.com/books/content?id=FcqNEAAAQBAJ&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "Ulysses":"https://books.google.com/books/content?id=AI1FvgAACAAJ&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "The Count of Monte Cristo":"https://books.google.com/books/content?id=QAa5l_8DNbcC&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "The Art of War":"https://books.google.com/books/content?id=uXIC0QEACAAJ&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "A Random Walk Down Wall Street":"https://books.google.com/books/content?id=CWxfkOBQ8VMC&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api",
+  "Empire of the Summer Moon":"https://books.google.com/books/content?id=mpEBZLxaLJQC&printsec=frontcover&img=1&zoom=2&edge=curl&source=gbs_api"
+};
